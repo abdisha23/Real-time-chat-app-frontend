97b809db8cf789c5d6b5ccbebd11a930f243c6f8
Abdi <ab_cs2048@yahoo.com>
2024-08-03 12:42:35 +0300

    modified the end point

--- src/utils/Services.js
+++ src/utils/Services.js
@@ -1,4 +1,4 @@
-export const baseUrl = "http://localhost:4000/api";
+export const baseUrl = "https://real-time-chat-app-backend-a5up.onrender.com/api";
 export const postRequest = async(url, body) => {
     const response = await fetch(url, {
         method: "POST",
